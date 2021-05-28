78d4332164f86fe2ef7a71fc87afbc22eed903fc
hussein010 <abouhachemhussein@gmail.com>
2021-05-29 02:35:40 +0300

    Step 8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,11 @@
+var onclick = document.getElementById("validate") 
+           
+onclick.addEventListener("click", function() { 
+var x =  document.getElementById("age").value;
+if (x > 18){
+    alert("you are over 18"); 
+}
+else {
+    alert("you are under 18"); ``
+}
+ });      
